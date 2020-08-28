adbbc245e82183d25c8316b65961694df7bbfba6
Dackelmaster <62453935+Dackelmaster@users.noreply.github.com>
2020-08-28 14:43:44 +0200

    Initial commit

---
+++ 20-08-2020/sended/index.php
@@ -0,0 +1,30 @@
+<style>
+       body{background-color: darkslategrey;}
+    h1{color: lightgray;}
+    label{color: lightgray;}
+    a:hover, a:visited, a{color: green;
+    font-size: large;}
+    a:hover{font-size: larger;
+    color: red;}
+</style>
+<center>
+<?php
+
+
+
+if ($_GET['geld'] >= 10) {
+    echo ("<h1>Ins Kino gehen!</h1>");
+    
+}elseif ($_GET['geld'] <= 10) {
+    echo ("<h1>Zuhause bleiben!</h1>");       
+}else {
+    echo ("<h1>Ein Fehler ist aufgetreten...</h1>");
+}
+
+
+?>
+
+
+
+<a href="../">Zurück zur Liste</a>
+</center>
